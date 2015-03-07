b4758d81cc02a2f46808d92fe2e369206c5b8da6
Carlos André <carlos.ulrich@gmail.com>
2015-03-07 19:49:23 +0000

    Fix failing spec

--- test/badge_render_test.js
+++ test/badge_render_test.js
@@ -6,7 +6,7 @@ describe('CodeBadge', function(){
     it('should render a stable codebadge', function(done) {
       var args = [ 'build', 'stable', '" passing "' ]
       var opts = [ '--filename stable-badge', '--directory ' + SCREENSHOTS_DIR ]
-      var cmd = 'phantomjs codebadge.js ' + args.join(" ") + opts.join(" ");
+      var cmd = 'phantomjs codebadge.js ' + args.join(" ") + " " + opts.join(" ");
       if(shell.exec(cmd).code == 0) {
         done();
       }
@@ -15,7 +15,7 @@ describe('CodeBadge', function(){
     it('should render a warning codebadge ', function(done) {
       var args = [ 'build', 'warning', '" 5 tests are failing "' ]
       var opts = [ '--filename warning-badge', '--directory ' + SCREENSHOTS_DIR ]
-      var cmd = 'phantomjs codebadge.js ' + args.join(" ") + opts.join(" ");
+      var cmd = 'phantomjs codebadge.js ' + args.join(" ") + " " + opts.join(" ");
       if(shell.exec(cmd).code == 0) {
         done();
       }
@@ -24,7 +24,7 @@ describe('CodeBadge', function(){
     it('should render a critical codebadge ', function(done) {
       var args = [ 'build', 'critical', '" codemageddon has arrived! "' ]
       var opts = [ '--filename critical-badge', '--directory ' + SCREENSHOTS_DIR ]
-      var cmd = 'phantomjs codebadge.js ' + args.join(" ") + opts.join(" ");
+      var cmd = 'phantomjs codebadge.js ' + args.join(" ") + " " + opts.join(" ");
       if(shell.exec(cmd).code == 0) {
         done();
       }
@@ -33,7 +33,7 @@ describe('CodeBadge', function(){
     it('should render an unknown codebadge ', function(done) {
       var args = [ 'build', 'unknown', '" no build information was provided "' ]
       var opts = [ '--filename unknown-badge', '--directory ' + SCREENSHOTS_DIR ]
-      var cmd = 'phantomjs codebadge.js ' + args.join(" ") + opts.join(" ");
+      var cmd = 'phantomjs codebadge.js ' + args.join(" ") + " " + opts.join(" ");
       if(shell.exec(cmd).code == 0) {
         done();
       }
